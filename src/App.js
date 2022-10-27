c1629089553fed61049d40a5e5ac428754167079
Luke Oguro <lukeoguro@gmail.com>
2022-10-27 16:27:30 +0900

    Setup json-server backend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Filter from './components/Filter';
 import ContactForm from './components/ContactForm';
 import Contacts from './components/Contacts';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [query, setQuery] = useState('');
   const contactsToShow = getContactsToShow();
+
+  useEffect(() => {
+    const request = axios.get("http://localhost:3001/contacts");
+    request.then(response => setContacts(response.data));
+  }, []);
 
   function getContactsToShow() {
     if (query) {
